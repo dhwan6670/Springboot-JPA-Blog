9b077a91e544886e9317c5cea46eb92ee10da88b
dhwan6670 <ndh521@naver.com>
2021-02-24 18:29:26 +0900

    시큐리티 사용한 로그인

--- src/main/java/com/cos/blog/config/SecurityConfig.java
+++ src/main/java/com/cos/blog/config/SecurityConfig.java
@@ -1,36 +1,53 @@
 package com.cos.blog.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import com.cos.blog.config.auth.PrincipalDetailService;
+
 //빈 등록 : 스프링 컨테이너에서 객체를 관리할 수 있게 하는 것
 
-@Configuration	//빈 등록(IoC관리)
-@EnableWebSecurity	// 시큐리티 필터가 등록이 된다.
-@EnableGlobalMethodSecurity(prePostEnabled = true) //특정 주소를 접근 하면 권한 및 인증을 미리 체크하겠다는 뜻.
-public class SecurityConfig extends WebSecurityConfigurerAdapter{		//필터 설정
+@Configuration // 빈 등록(IoC관리)
+@EnableWebSecurity // 시큐리티 필터가 등록이 된다.
+@EnableGlobalMethodSecurity(prePostEnabled = true) // 특정 주소를 접근 하면 권한 및 인증을 미리 체크하겠다는 뜻.
+public class SecurityConfig extends WebSecurityConfigurerAdapter { // 필터 설정
+
+	@Autowired
+	private PrincipalDetailService principalDetailService;
 	
 	@Bean // IoC가 됨.
 	public BCryptPasswordEncoder encodePWD() {
 		return new BCryptPasswordEncoder();
 	}
+
+// 시큐리티가 대신 로그인해줘 password를 가로채기를 하는데
+// 해당 password가 무엇을 해쉬가 되어 회원가입이 되었는지 알아야
+//	같은 해쉬로 암호화해서 DB에 있는 해쉬랑 비교할 수 있음.
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(principalDetailService).passwordEncoder(encodePWD());
+	}
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
-			.csrf().disable() //csrf 토큰 비활성화(테스트시 걸어두는 게 좋음)
-			.authorizeRequests()
-				.antMatchers("/", "/auth/**", "/js/**", "/css/**", "/image/**") //auth로 들어노는것
-				.permitAll()						//무엇이든 허용
-				.anyRequest()	// 아닌 다른 요청
-				.authenticated()	//인증이 되어야 허용
+			.csrf().disable() // csrf 토큰 비활성화(테스트시 걸어두는 게 좋음)
+				.authorizeRequests()
+				.antMatchers("/", "/auth/**", "/js/**", "/css/**", "/image/**") // auth로 들어노는것
+				.permitAll() // 무엇이든 허용
+				.anyRequest() // 아닌 다른 요청
+				.authenticated() // 인증이 되어야 허용
 			.and()
 				.formLogin()
-				.loginPage("/auth/loginForm");
+				.loginPage("/auth/loginForm")
+				.loginProcessingUrl("/auth/loginProc")
+				.defaultSuccessUrl("/"); // 스프링 시큐리티가 해당 주소로 요청오는 로그인을 가로채서 대신 로그인 해준다.
 	}
 }
---
+++ src/main/java/com/cos/blog/config/auth/PrincipalDetail.java
@@ -0,0 +1,70 @@
+package com.cos.blog.config.auth;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.cos.blog.model.User;
+
+// 스프링 시큐리티가 로그인 요청을 가로채서 로그인을 진행하고 완료가 되며 UserDetils 타입의 오브젝트를
+// 스프링 시큐리티의 고유한 세션저장소에 저장을 해준다.
+public class PrincipalDetail implements UserDetails{
+	private User user;  //컨포지션
+
+	public PrincipalDetail(User user) {
+		this.user = user;
+	}
+	@Override
+	public String getPassword() {
+		return user.getPassword();
+	}
+
+	@Override
+	public String getUsername() {
+		return user.getUsername();
+	}
+
+	// 계정이 만료되지 않았는지 리턴한다 (true : 만료안됨)
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+	
+	// 계정이 잠겨있는지 않았는지 리턴한다 (true : 잠기지 않음)
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	// 비민번호가 만료되지 않았는지 리턴한다 (true: 만료안됨)
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	//계정이 활성화(사용가능)인지 리턴한다 (ture : 활성화)
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
+
+	// 계정이 갖고있는 권한 목록을 리턴한다.(권한이 여러개 있을 수 있어서 루프를 돌아야 함) //현재 프로젝트에서는 한개만
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		
+		Collection<GrantedAuthority> collectors = new ArrayList<>();
+//		collectors.add(new GrantedAuthority() {
+//			
+//			@Override
+//			public String getAuthority() {
+//				return "ROLE_" + user.getRole(); //ROEL_스프링 규칙 //리턴되면 ROLE_USER
+//			}
+//		});
+		collectors.add(()->{return "ROLE_" + user.getRole();});
+		
+		return collectors;
+	}
+	
+}
